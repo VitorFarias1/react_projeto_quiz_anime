b24f4a54fefdf48ff0c6d66e1d7ede7e28fa4ce5
VitorFarias1 <vitor.rodriguesfarias@gmail.com>
2024-08-29 12:19:28 -0300

    wip: pág de perguntas e pág de boas vindas modificadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import { useContext } from 'react'
+import { QuizContext } from './context/quiz'
+import { useEffect } from 'react'
+import Question from './components/Question'
 import Welcome from './components/Welcome'
 
 import './App.css'
 
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch( {type: "REORDER_QUESTIONS"});
+  },[])
 
   return (
     <div className="App">
       <h1>Quiz de Anime</h1>
-      <Welcome />
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   )
 }
