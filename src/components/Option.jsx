41dfd95231a102f8d6142bf622227bc0797e25f1
VitorFarias1 <vitor.rodriguesfarias@gmail.com>
2024-08-31 19:10:56 -0300

    feat: quiz funcional

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -7,7 +7,11 @@ const Option = ({ option, selectOption, answer }) => {
     const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className="option" onClick={() => selectOption(option)}>
+    <div className={`option ${
+        quizState.answerSelected && option === answer ? "correct" : "not-selected"
+        } ${
+            quizState.answerSelected && option !== answer ? "wrong" : "not-selected"
+        }`} onClick={() => selectOption(option)} >
         <p>{option}</p>
     </div>
   )
